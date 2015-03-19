1ad822c4bc7a8182a9819dbb05c8610d93cf1e5a
Ricardo <rick@reddraft.com>
2015-03-19 00:04:58 -0400

    generated Day and event model,controllers and views

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,4 +24,3 @@ Event.create(name: "Casual Friday", from: DateTime.parse('16th Jan 2015 00:00 AM
 Event.create(name: "Running", from: DateTime.parse('16th Jan 2015 08:00 PM'), to: DateTime.parse('16th Jan 2015 08:30 PM'), location: "", day_id: d6.id)
 Event.create(name: "Grocery Shopping", from: DateTime.parse('16th Jan 2015 09:00 PM'), to: DateTime.parse('16th Jan 2015 09:30 AM'), location: "", day_id: d6.id)
 
-d6 = Day.create(date: DateTime.parse('17th Jan 2015 00:00 AM'))
